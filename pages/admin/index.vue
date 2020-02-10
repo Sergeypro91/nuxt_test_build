ae810bb3e7b3ef02d987c5800a98e995e41f185f
Sergey Shevtsov <sergeyprostoas@gmail.com>
2020-02-10 17:32:43 +0300

    Fish 2/3

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,12 +1,37 @@
 <template>
-  <h1>Welcome to Amin page!</h1>
+  <div>
+    <h1>Posts analytics</h1>
+    <AppAnalyticChart
+      title="Views count"
+      :labels="views.labels"
+      :data="views.data"
+    >
+    </AppAnalyticChart>
+
+    <AppAnalyticChart
+      title="Comments count"
+      :labels="comments.labels"
+      :data="comments.data"
+    >
+    </AppAnalyticChart>
+  </div>
 </template>
 
 <script>
+import AppAnalyticChart from '@/components/admin/AnalyticChart'
+
 export default {
+  components: { AppAnalyticChart },
+
   layout: 'admin',
 
-  middleware: ['admin-auth']
+  middleware: ['admin-auth'],
+
+  async asyncData({ store }) {
+    const { views, comments } = await store.dispatch('post/getAnalytics')
+
+    return { views, comments }
+  }
 }
 </script>
 
