531e3092ba2d1df6abde3955f1ced4554c069266
Sergey Shevtsov <sergeyprostoas@gmail.com>
2020-02-11 17:14:37 +0300

    Prepare to deploy 2:

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,9 @@ module.exports = {
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
-  axios: {},
+  axios: {
+    baseURL: process.env.BASE_URL || 'http://localhost:3000'
+  },
 
   env: {
     appName: 'SSR Blog'
